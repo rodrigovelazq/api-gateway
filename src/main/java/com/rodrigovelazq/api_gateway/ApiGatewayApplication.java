d77e69058fe0c7021bdd4a28a27096d6090654c0
rodrigovelazq <rodrigo.24.06.90@gmail.com>
2025-03-10 20:24:23 -0300

    Add observability

--- src/main/java/com/rodrigovelazq/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/rodrigovelazq/api_gateway/ApiGatewayApplication.java
@@ -1,27 +1,73 @@
 package com.rodrigovelazq.api_gateway;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
-	@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route("product-service", r -> r.path("/products/**")
-						.uri("lb://PRODUCT-SERVICE"))
-				.route("cart-service", r -> r.path("/carts/**")
-						.uri("lb://CART-SERVICE"))
-				.build();
-	}
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("product_service", r -> r.path("/rodrigovelazq/products/**")
+                        .filters(f -> f.rewritePath("/rodrigovelazq/products", "/products")
+                                .rewritePath("/rodrigovelazq/products/(?<segment>.*)", "/products/${segment}")
+                                .circuitBreaker(config -> config.setName("productsCircuitBreaker")
+                                        .setFallbackUri("forward:/contactSupport"))
+                        ).uri("lb://PRODUCT-SERVICE"))
+                .route("cart_service", r -> r.path("/rodrigovelazq/carts/**")
+                        .filters(f -> f.rewritePath("/rodrigovelazq/carts", "/carts")
+                                .rewritePath("/rodrigovelazq/carts/(?<segment>.*)", "/carts/${segment}")
+
+                                .circuitBreaker(config -> config.setName("cartsCircuitBreaker")
+                                        .setFallbackUri("forward:/contactSupport"))
+
+                                .retry(retryConfig -> retryConfig.setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+
+                                .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+                                        .setKeyResolver(userKeyResolver()))
+                        ).uri("lb://CART-SERVICE"))
+                .build();
+    }
+
+    /*@Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
+    }*/
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(1, 1, 1);
+    }
+
+    @Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+                .defaultIfEmpty("anonymous");
+    }
+
 
 }
 
